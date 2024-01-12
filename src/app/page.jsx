f271b38104ec2eb96652b9d199852e4d993e7a02
DylanQuenon <dylan.quenon.04@gmail.com>
2024-01-12 17:22:01 +0100

    installation initiale

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './style.module.css'
+
+export default function Home() {
+  return (
+    <>
+    <div className="slide">
+      
+    </div>
+    </>
+  )
+}
