2dfb5fb49fc4a431359f91ac941a02a82070b342
DylanQuenon <dylan.quenon.04@gmail.com>
2024-01-13 16:37:27 +0100

    page home

--- src/app/Components/NavBar.jsx
+++ src/app/Components/NavBar.jsx
@@ -1,18 +1,51 @@
+// Navbar component
 'use client'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from './navbar.module.scss';
 
 export default function NavBar() {
   const [open, setOpen] = useState(false);
+  const [scrollProgress, setScrollProgress] = useState(0);
 
   const handleMenuClick = () => {
     setOpen(!open);
   };
 
+  useEffect(() => {
+    const header = document.querySelector(`.${styles.navbar}`);
+    const progressbar = document.getElementById(styles.progressbar);
+    const lastScrollValue = { current: 0 };
+
+    const handleScroll = () => {
+      const top = document.documentElement.scrollTop;
+      if (lastScrollValue.current < top) {
+        header.classList.add(styles.hide);
+        header.classList.remove(styles.show);
+      } else {
+        header.classList.remove(styles.hide);
+        header.classList.add(styles.show);
+      }
+      lastScrollValue.current = top;
+
+      //calculer la progression de la barre
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight - windowHeight;
+      const progressWidth = (top / documentHeight) * 100;
+      setScrollProgress(progressWidth);
+      progressbar.style.width = `${scrollProgress}%`;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <header className={styles.navbar}>
+      <header className={`${styles.navbar} ${styles.show}`}>
         <nav>
           <ul>
             <li>
@@ -40,6 +73,8 @@ export default function NavBar() {
         <div className={`${styles.bar} ${open ? styles.openBurger2 : ''}`}></div>
         <div className={`${styles.bar} ${open ? styles.openBurger3 : ''}`}></div>
       </div>
+
+      <div id={styles.progressbar} style={{ width: `${scrollProgress}%` }}></div>
 
       <div id={styles.menuMobile} className={`${styles.menuMobile} ${open ? styles.openMenu : ''}`}>
         <nav>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,54 @@
 import Image from 'next/image'
-import styles from './style.module.scss'
+import './globals.scss'
+import { ArrowRightIcon } from '@heroicons/react/outline'
 
 
 export default function Home() {
   return (
     <>
-    <div className="slide">
-      
+    <div className="slide" id='home'>
+      <div className="circle" id='circle_home'></div>
+      <div className="wrapper">
+        <div className="block-home">
+
+          <div className="jumbotron">
+            <h1>BOOKS LEAGUE</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae distinctio maxime eaque dolorum doloribus possimus.</p>
+            <div className="link_more">
+              <a href="/presentation" className='button_anim'>Voir plus <span><ArrowRightIcon className='icon'/></span></a>
+            </div>
+          <div className="circle" id='circle_bottom'></div>
+          </div>
+
+        <div className="block-right">
+          <div className="pic_rectangle">
+            <Image src="/images/coverbook.jpg" alt="Book" width={300} height={350} className='picBook' />
+          </div>
+        </div>
+
+        </div>
+      </div>
+        <div className="bandeau">
+          <div className="block" id="block1">
+          <p> + 150</p>
+          <p> Livres</p>
+          </div>
+          <div className="block" id="block2">
+            <p> + 50</p>
+            <p> Biographies</p>
+          </div>
+          <div className="block" id="block3">
+            <p> 100%</p>
+            <p> Passion</p>
+          </div>
+          
+        </div>
+
     </div>
+    <div className="slide">
+
+    </div>
+  
     </>
   )
 }
